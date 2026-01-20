622387f37902554e791b324e1281ced405835256
hqcoder05 <hoangquocfw@gmail.com>
2026-01-20 14:54:49 +0700

    update function

--- authors-service/src/main/java/com/bookstore/authorsservice/repository/AuthorRepository.java
+++ authors-service/src/main/java/com/bookstore/authorsservice/repository/AuthorRepository.java
@@ -1,14 +1,36 @@
 package com.bookstore.authorsservice.repository;
 
 import com.bookstore.authorsservice.entity.Author;
+import com.bookstore.authorsservice.enums.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-    Optional<Author> findByEmail(String email);
-    List<Author> findByCountry(String Country);
+public interface AuthorRepository extends JpaRepository<Author, UUID> {
+
+    List<Author> findByFullnameContainingIgnoreCase(String fullname);
+
+    Page<Author> findByFullnameContainingIgnoreCase(
+            String fullname,
+            Pageable pageable
+    );
+
+    List<Author> findByStatus(Status status);
+
+    Optional<Author> findByUuidAndStatus(UUID uuid, Status status);
+
+    boolean existsByFullnameIgnoreCase(String fullname);
+
+    List<Author> findByFullnameContainingIgnoreCaseAndStatus(
+            String fullname,
+            Status status
+    );
 }
+
+
