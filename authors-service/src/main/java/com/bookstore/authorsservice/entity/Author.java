8d11bb03aacee88d29e3dbd8252f1cc4fd7210f2
hqcoder05 <hoangquocfw@gmail.com>
2026-01-20 14:44:05 +0700

    Change ID -> UUID. Add more information for authors

--- authors-service/src/main/java/com/bookstore/authorsservice/entity/Author.java
+++ authors-service/src/main/java/com/bookstore/authorsservice/entity/Author.java
@@ -1,5 +1,6 @@
 package com.bookstore.authorsservice.entity;
 
+import com.bookstore.authorsservice.enums.Status;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -8,9 +9,16 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
-@Table(name = "authors")
+@Table(
+        name = "authors",
+        indexes = {
+                @Index(name = "idx_author_fullname", columnList = "fullname"),
+                @Index(name = "idx_author_status", columnList = "status")
+        }
+)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,23 +26,32 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class) // Tự động điền ngày tạo/sửa
 public class Author {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue
+    @Column(nullable = false, updatable = false)
+    private UUID uuid;
 
     @Column(nullable = false)
-    private String name;
+    private String fullname;
 
-    @Column(unique = true)
-    private String email;
+    private String penName;
+
+    private LocalDate dateOfBirth;
+
+    private LocalDate dateOfDeath;
+
+    private String nationality;
 
     @Column(length = 2000)
-    private String bio;
+    private String biography;
 
-    private LocalDate birthDate; // Ngày sinh (YYYY-MM-DD)
+    private String avatarUrl;
 
-    private String country;
-    private String website;
-    private String imageUrl;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private boolean verified = false;
 
     @CreatedDate
     @Column(updatable = false)
---
+++ authors-service/src/main/java/com/bookstore/authorsservice/enums/Status.java
@@ -0,0 +1,4 @@
+package com.bookstore.authorsservice.enums;
+
+public enum Status {
+}
