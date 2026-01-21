261589ce5aa2f05138143f1ac5b7bed2863de2aa
hqcoder05 <hoangquocfw@gmail.com>
2026-01-21 10:36:39 +0700

    Add NoContructors and All Contructors

--- authors-service/src/main/java/com/bookstore/authorsservice/dto/response/AuthorResponse.java
+++ authors-service/src/main/java/com/bookstore/authorsservice/dto/response/AuthorResponse.java
@@ -1,12 +1,16 @@
 package com.bookstore.authorsservice.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorResponse {
     private UUID uuid;
     private String fullname;
