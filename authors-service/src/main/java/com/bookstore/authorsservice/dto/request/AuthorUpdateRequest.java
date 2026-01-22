d3782ae42dc01cd68efa79ba0f4aa7ccb26ecd2d
hqcoder05 <hoangquocfw@gmail.com>
2026-01-22 21:43:17 +0700

    add Builder, No-All Contructors

--- authors-service/src/main/java/com/bookstore/authorsservice/dto/request/AuthorUpdateRequest.java
+++ authors-service/src/main/java/com/bookstore/authorsservice/dto/request/AuthorUpdateRequest.java
@@ -1,10 +1,16 @@
 package com.bookstore.authorsservice.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorUpdateRequest {
     private String fullname;
     private String penName;
