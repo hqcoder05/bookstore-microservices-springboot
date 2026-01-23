69452a379144d7db7e0f9c62f1d3d25fa0454629
hqcoder05 <hoangquocfw@gmail.com>
2026-01-23 21:07:34 +0700

    jdk 21

--- authors-service/src/main/java/com/bookstore/authorsservice/entity/Author.java
+++ authors-service/src/main/java/com/bookstore/authorsservice/entity/Author.java
@@ -46,11 +46,13 @@ public class Author {
 
     private String avatarUrl;
 
+    @Builder.Default
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Status status;
+    private Status status = Status.ACTIVE;
 
     @Column(nullable = false)
+    @Builder.Default
     private boolean verified = false;
 
     @CreatedDate
