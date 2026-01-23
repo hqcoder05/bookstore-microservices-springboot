7e1ae741faa89c9d54664a10f4c75a8299230b83
hqcoder05 <hoangquocfw@gmail.com>
2026-01-23 20:33:42 +0700

    Open Get Method

--- authors-service/src/main/java/com/bookstore/authorsservice/client/BookClient.java
+++ authors-service/src/main/java/com/bookstore/authorsservice/client/BookClient.java
@@ -1,8 +1,14 @@
 package com.bookstore.authorsservice.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.List;
 import java.util.UUID;
 
+@FeignClient(name = "books-service")
 public interface BookClient {
+
+    @GetMapping("/books/author/{authorId}")
     List<BookResponse> getBooksByAuthor(UUID authorId);
 }
