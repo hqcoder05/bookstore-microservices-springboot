35eb70656fed4e22d1f5f9d2d49bbaee4b3bdd44
hqcoder05 <hoangquocfw@gmail.com>
2026-01-23 20:35:22 +0700

    Foregin Client

--- authors-service/src/main/java/com/bookstore/authorsservice/AuthorsServiceApplication.java
+++ authors-service/src/main/java/com/bookstore/authorsservice/AuthorsServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.bookstore.authorsservice.client")
 public class AuthorsServiceApplication {
 
     public static void main(String[] args) {
